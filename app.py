8e247f54e6e325b042d0d4cf774348f7f0957fe4
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-13 18:37:45 -0300

    Add: Flask Login

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from models.user import User
 from database import db
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "YOUR_KEY"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///YOUR_DATABASE_NAME.db'
 
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():

15370ab5b41f35c811c7cebad23d76887376fb4b
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-13 18:39:15 -0300

    Add: Starting the creation of the login route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager
@@ -12,6 +12,18 @@ login_manager = LoginManager()
 
 db.init_app(app)
 login_manager.init_app(app)
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        pass
+
+    return jsonify({"message": "Credenciais inválidas!"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():

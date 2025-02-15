c912dd5ffbce093991458797761c2b566faf1025
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-15 20:06:40 -0300

    Add: Creating a user and performing authentication

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 
@@ -13,6 +13,12 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route("/login", methods=["POST"])
 def login():
     data = request.json
@@ -22,7 +28,8 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
-                return jsonify({"message": "Autenticação realizada com sucesso!"})
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso!"})
     return jsonify({"message": "Credenciais inválidas!"}), 400
 
 @app.route("/hello-world", methods=["GET"])

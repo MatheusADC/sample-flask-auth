905b778739a4a1b42b401c863fcd31e99dde58d6
MatheusSuper <matheusamaralsuper@gmail.com>
2025-02-16 06:12:21 -0300

    Remove: hello-world route

--- app.py
+++ app.py
@@ -38,9 +38,5 @@ def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesso!"})
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-    return "Hello World!"
-
 if __name__ == '__main__':
     app.run(debug=True)
